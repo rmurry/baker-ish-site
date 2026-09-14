cf1c92816bd108129771e9ef807bf9bd1b9592a8
rmurry <rylanmurry@gmail.com>
2026-09-13 22:21:03 -0500

    removed png to test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import gallery1 from './assets/gallery/choc_chip_lemon.jpg'
 import gallery2 from './assets/gallery/orange_funfetti.JPG'
 import gallery3 from './assets/gallery/football2.jpg'
 import gallery4 from './assets/gallery/mahjong.jpg'
-import gallery5 from './assets/gallery/banana_nut2.PNG'
 
 const galleryImages = [
   {
@@ -22,9 +21,6 @@ const galleryImages = [
   },
   {
     src: gallery4
-  },
-  {
-    src: gallery5
   }
 ]
 
